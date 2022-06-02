361d8d820025da6d78844a2e7e5eef6a6cd339d5
devtruetecnologia <95711563+devtruetecnologia@users.noreply.github.com>
2022-06-02 09:29:58 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -30,6 +30,7 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <meta name="facebook-domain-verification" content="ylxfmvn23ypa187ko3oxmohxdldglj" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,800;1,500&family=DM+Sans:wght@700&display=swap" rel="stylesheet" />
                     
